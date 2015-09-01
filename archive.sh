dec0fc17a3bd9939c02613775af944eb4550753c
Rafael Giusti <rfgiusti@gmail.com>
2015-09-01 08:41:02 -0300

    Adds usage line for archive; license headers

--- archive.sh
+++ archive.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# archive
+# This will attempt to archive the contents of a directory into a .tar.gz file
+# with the same base name as the directory, and then remove the directory
+# files.
+#
+# Copyright 2015 "Rafael Giusti" (rfgiusti@gmail.com)
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# This software is on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. Use at your own risk.
+
+if [[ "$1" == "" ]]; then
+	echo "Usage $0  <dir1> [<dir2> ...]"
+	exit 1
+fi
+
 for i in "$@"; do
 	dirname="$i"
 	if [[ ! -d "$dirname" ]]; then
